7bbd0bee7b8a15b5320f7850bf7c26396cfbf5af
JUDAS89 <lorena.peroza@outlook.com>
2024-03-31 07:28:18 -0300

    limpiar el home al volver

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,17 @@
+import React, { useEffect } from "react";
+import { usePokemon } from "../context/PokemonContext"
 import presentacionImage from "../assets/He.webp"
 
 const Home = () => {
+    const { setPokemonDetails } = usePokemon();
+
+    // Función para limpiar los detalles del Pokémon
+    useEffect(() => {
+        return () => {
+            setPokemonDetails(null);
+        };
+    }, []);
+
     return (
         <div className="presentacionHome">
             <h1>Bienvenido maestro pokemón</h1>
